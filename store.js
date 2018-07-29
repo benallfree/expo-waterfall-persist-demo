93a766ab4157ddf258f9f3b526079babc0f9e048
Ben Allfree <ben@benallfree.com>
2018-07-28 22:24:54 -0700

    Reset store

--- store.js
+++ store.js
@@ -2,7 +2,7 @@ import createStore from 'react-waterfall'
 import { persist } from 'expo-waterfall-persist'
 
 const config = {
-  initialState: { version: 4, greeting: 'Hello world!', tick: 0 },
+  initialState: { version: 1, greeting: 'Hello world!', tick: 0 },
   actionsCreators: {
     setGreeting: (state, actions, greeting) => ({ greeting }),
     tick: ({ tick }) => ({
